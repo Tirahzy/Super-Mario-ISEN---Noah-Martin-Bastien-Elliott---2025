8166f378a7e8d1d4e5f2e0d8605c00c6860074b2
Martin Hurvois <martin@hurvois.fr>
2025-03-20 09:10:40 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/projetMario.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projetMario.dir/projetMario/Affichage.c.obj"
+  "CMakeFiles/projetMario.dir/projetMario/Affichage.c.obj.d"
+  "CMakeFiles/projetMario.dir/projetMario/deplacement.c.obj"
+  "CMakeFiles/projetMario.dir/projetMario/deplacement.c.obj.d"
+  "CMakeFiles/projetMario.dir/projetMario/main.c.obj"
+  "CMakeFiles/projetMario.dir/projetMario/main.c.obj.d"
+  "libprojetMario.dll.a"
+  "projetMario.exe"
+  "projetMario.exe.manifest"
+  "projetMario.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projetMario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
